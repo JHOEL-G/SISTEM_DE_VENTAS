a0b56d5dd2a8ca2b330f393b66fe5a19f62f0d74
JHOEL-G <juniorgarciapro2020@gmail.com>
2025-06-19 17:32:41 -0500

    datos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegura que Vercel sepa dónde buscar los archivos generados
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
